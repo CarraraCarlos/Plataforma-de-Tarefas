8385258086648ea66af460afd857feba27ee1423
Carlos Carrara <carlinhos.alberto308@gmail.com>
2026-02-24 10:47:20 -0300

    Primeiro Commit - Projeto criado.

---
+++ PlataformaDeTarefas/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlataformaDeTarefas.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
