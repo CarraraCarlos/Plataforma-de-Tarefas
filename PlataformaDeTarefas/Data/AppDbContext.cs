b8ada08dee37b783559cf260b6c01d9a6882d801
Carlos Carrara <carlinhos.alberto308@gmail.com>
2026-03-03 10:50:59 -0300

    Commit request * string connect *add new containers

---
+++ PlataformaDeTarefas/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PlataformaDeTarefas.Models;
+
+namespace PlataformaDeTarefas.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<AppUser> AppUsers { get; set; }
+    public DbSet<ToDo> ToDos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<AppUser>().ToTable("users");
+        builder.Entity<IdentityRole>().ToTable("roles");
+        builder.Entity<IdentityUserRole<string>>().ToTable("user_roles");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("user_claims");
+        builder.Entity<IdentityUserToken<string>>().ToTable("user_tokens");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("user_logins");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("user_claims");
+    }
+}
